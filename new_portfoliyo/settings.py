646f354736187e1c32bffabc0d7c13623d039991
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-10-14 18:50:38 +0530

    MyWebsite

--- new_portfoliyo/settings.py
+++ new_portfoliyo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*e@ybu93_ek@0a&v3a5&8h$@8i542lbdfwyh_fmw*8+11g)z+%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'new_portfoliyo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
